c975f385fdbc04ac4eb53fd058b9b319e41ce141
Alex <alexhandel96@googlemail.com>
2019-07-13 20:11:13 -0300

    first commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class playerController : MonoBehaviour
+{
+    public float health;
+    public float attack;
+    public float defense;
+    public TextMeshProUGUI text;
+    public GameObject currentEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (currentEnemy == null)
+        {
+            currentEnemy = GameObject.FindWithTag("Enemy");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateText(text);
+        if (currentEnemy!=null)
+        {
+            if (Input.anyKeyDown)
+            {
+                currentEnemy.GetComponent<enemyController>().health -= (attack - currentEnemy.GetComponent<enemyController>().defense);
+            }
+        }
+        
+    }
+
+    void updateText(TextMeshProUGUI text)
+    {
+        text.SetText("Player \nHealth: " + health + "\nAttack: " + attack + "\nDefense: " + defense);
+    }
+}
